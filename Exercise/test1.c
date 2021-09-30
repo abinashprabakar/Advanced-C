ab1fc3ec73b12772841d9902c75f661ce9aaf9ab
abinashprabakar <abinashprabakaran@gmail.com>
2021-09-30 09:20:15 +0530

    test Exercise

--- Exercise/test1.c
+++ Exercise/test1.c
@@ -16,30 +16,30 @@ struct node
 int ll_min(struct node **ptr) 
 {
 	struct node *temp = *ptr;	/* assigning temp to head ptr */
-	int *min;
-
+	int min;
+	min = *temp->num;
 	/* temp pointing to the next address */
 	temp = temp -> next;
 	if(ptr == NULL)
 		printf("List is empty\n");
 	else
 	{
-		min = temp -> num;	/* storing the node num to min  */
+        	/* storing the node num to min  */
 		while(temp)
 		{
 			/* if the min number greater than the temp->num, store temp->num in min */
-			if(min > temp->num)	
+			if(min > *temp->num)	
 			{
-				min = temp -> num;
+				min = *temp -> num;
 			}
 			temp = temp -> next;	/* pointing to the next address */
 		}
-		return *min;
+		return min;
 	}
 }
 int main() 
 {
-	int x = 258, y = 207, z = 209;
+	int x = 207, y = 209, z = 258;
     	struct node n1, n2, n3;
     	struct node *ptr = &n1;
     	n1.num = &x;
